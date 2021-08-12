fd7a8bb8ef33890f97e23395bd8ff1261028e29b
Jelani Canty <jelani.canty@peapoddigitallabs.com>
2021-08-12 10:12:47 -0500

    add course action const

--- src/redux/actions/courseActions.js
+++ src/redux/actions/courseActions.js
@@ -1,4 +1,6 @@
+import { CREATE_COURSE } from "./actionTypes";
+
 //actions must have types
 export function createCourse(course) {
-    return { type: "CREATE_COURSE", course}
+    return { type: CREATE_COURSE, course}
 }
